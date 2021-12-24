b9306903511aed8a7e426512a3e25f452d45a8db
Kirill Dubovikov <kdubovikov@cinimex.ru>
2021-12-24 14:09:38 +0300

    feat 19 staging buffer

--- src/main.rs
+++ src/main.rs
@@ -685,7 +685,7 @@ impl VulkanApp {
 
         let sync_objects = VulkanApp::create_sync_objects(&device);
 
-        let (vertex_buffer, vertex_buffer_memory) = VulkanApp::create_vertex_buffer(&instance, &device, physical_device);
+        let (vertex_buffer, vertex_buffer_memory) = VulkanApp::create_vertex_buffer(&instance, &device, physical_device, command_pool, graphics_queue);
         let command_buffers = VulkanApp::create_command_buffers(&device, command_pool, graphics_pipeline, &swapchain_framebuffers, render_pass, swapchain_stuff.extent, vertex_buffer);
 
         VulkanApp {
@@ -1251,37 +1251,85 @@ impl VulkanApp {
         sync_objects
     } 
 
-    fn create_vertex_buffer(instance: &ash::Instance, device: &ash::Device, physical_device: vk::PhysicalDevice) -> (vk::Buffer, vk::DeviceMemory) {
-        let vertex_buffer_create_info = vk::BufferCreateInfo {
-            s_type: vk::StructureType::BUFFER_CREATE_INFO,
-            p_next: ptr::null(),
-            flags: vk::BufferCreateFlags::empty(),
-            size: std::mem::size_of_val(&VERTICES_DATA) as u64,
-            usage: vk::BufferUsageFlags::VERTEX_BUFFER,
-            sharing_mode: vk::SharingMode::EXCLUSIVE,
-            queue_family_index_count: 0,
-            p_queue_family_indices: ptr::null(),
-        };
-
-        let vertex_buffer = unsafe {
-            device
-                .create_buffer(&vertex_buffer_create_info, None)
-                .expect("Failed to create Vertex Buffer")
-        };
-
-        let mem_requirements = unsafe {
-            device.get_buffer_memory_requirements(vertex_buffer)
-        };
+    fn create_vertex_buffer(instance: &ash::Instance, device: &ash::Device, physical_device: vk::PhysicalDevice, command_pool: vk::CommandPool, submit_queue: vk::Queue) -> (vk::Buffer, vk::DeviceMemory) {
+        let buffer_size = std::mem::size_of_val(&VERTICES_DATA) as vk::DeviceSize;
 
         let mem_properties = unsafe {
             instance.get_physical_device_memory_properties(physical_device)
         };
 
-        let required_memory_flags = vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT;
+        let (staging_buffer, staging_buffer_memory) = VulkanApp::create_buffer(
+            device, 
+            buffer_size, 
+            vk::BufferUsageFlags::TRANSFER_SRC,
+            vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT, 
+            &mem_properties
+        ); 
+
+        unsafe {
+            let data_ptr = device
+                .map_memory(staging_buffer_memory, 0, buffer_size, vk::MemoryMapFlags::empty())
+                .expect("Failed to Map Memory") as *mut Vertex;
+
+            data_ptr.copy_from_nonoverlapping(VERTICES_DATA.as_ptr(), VERTICES_DATA.len());
+
+            device.unmap_memory(staging_buffer_memory);
+        }
+
+        let (vertex_buffer, vertex_buffer_memory) = VulkanApp::create_buffer(
+            device,
+            buffer_size,
+            vk::BufferUsageFlags::TRANSFER_DST | vk::BufferUsageFlags::VERTEX_BUFFER,
+            vk::MemoryPropertyFlags::DEVICE_LOCAL,
+            &mem_properties,
+        );
+
+        VulkanApp::copy_buffer(
+            device,
+            submit_queue,
+            command_pool,
+            staging_buffer,
+            vertex_buffer,
+            buffer_size,
+        );
+
+        unsafe {
+            device.destroy_buffer(staging_buffer, None);
+            device.free_memory(staging_buffer_memory, None);
+        }
+
+        (vertex_buffer, vertex_buffer_memory)
+    }
+
+    fn create_buffer(
+        device: &ash::Device,
+        size: vk::DeviceSize,
+        usage: vk::BufferUsageFlags,
+        required_memory_properties: vk::MemoryPropertyFlags,
+        device_memory_properties: &vk::PhysicalDeviceMemoryProperties,
+    ) -> (vk::Buffer, vk::DeviceMemory) {
+        let buffer_create_info = vk::BufferCreateInfo {
+            s_type: vk::StructureType::BUFFER_CREATE_INFO,
+            p_next: ptr::null(),
+            flags: vk::BufferCreateFlags::empty(),
+            size,
+            usage,
+            sharing_mode: vk::SharingMode::EXCLUSIVE,
+            queue_family_index_count: 0,
+            p_queue_family_indices: ptr::null(),
+        };
+
+        let buffer = unsafe {
+            device
+                .create_buffer(&buffer_create_info, None)
+                .expect("Failed to create Vertex Buffer")
+        };
+
+        let mem_requirements = unsafe { device.get_buffer_memory_requirements(buffer) };
         let memory_type = VulkanApp::find_memory_type(
             mem_requirements.memory_type_bits,
-            required_memory_flags,
-            mem_properties
+            required_memory_properties,
+            *device_memory_properties,
         );
 
         let allocate_info = vk::MemoryAllocateInfo {
@@ -1291,7 +1339,7 @@ impl VulkanApp {
             memory_type_index: memory_type,
         };
 
-        let vertex_buffer_memory = unsafe {
+        let buffer_memory = unsafe {
             device
                 .allocate_memory(&allocate_info, None)
                 .expect("Failed to allocate vertex buffer memory!")
@@ -1299,24 +1347,76 @@ impl VulkanApp {
 
         unsafe {
             device
-                .bind_buffer_memory(vertex_buffer, vertex_buffer_memory, 0)
+                .bind_buffer_memory(buffer, buffer_memory, 0)
                 .expect("Failed to bind Buffer");
-
-            let data_ptr = device
-                .map_memory(
-                    vertex_buffer_memory,
-                    0,
-                    vertex_buffer_create_info.size,
-                    vk::MemoryMapFlags::empty(),
-                )
-                .expect("Failed to Map Memory") as *mut Vertex;
-
-            data_ptr.copy_from_nonoverlapping(VERTICES_DATA.as_ptr(), VERTICES_DATA.len());
-
-            device.unmap_memory(vertex_buffer_memory);
         }
 
-        (vertex_buffer, vertex_buffer_memory)
+        (buffer, buffer_memory)
+    }
+
+    fn copy_buffer(device: &ash::Device, submit_queue: vk::Queue, command_pool: vk::CommandPool, src_buffer: vk::Buffer, dst_buffer: vk::Buffer, size: vk::DeviceSize) {
+        let allocate_info = vk::CommandBufferAllocateInfo {
+            s_type: vk::StructureType::COMMAND_BUFFER_ALLOCATE_INFO,
+            p_next: ptr::null(),
+            command_pool,
+            level: vk::CommandBufferLevel::PRIMARY,
+            command_buffer_count: 1,
+        };
+
+        let command_buffers = unsafe {
+            device
+                .allocate_command_buffers(&allocate_info)
+                .expect("Failed to allocate Command Buffer")
+        };
+
+        let command_buffer = command_buffers[0];
+        let begin_info = vk::CommandBufferBeginInfo {
+            s_type: vk::StructureType::COMMAND_BUFFER_BEGIN_INFO,
+            p_next: ptr::null(),
+            flags: vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT,
+            p_inheritance_info: ptr::null(),
+        };
+
+        unsafe {
+            device
+                .begin_command_buffer(command_buffer, &begin_info)
+                .expect("Failed to begin Command Buffer");
+
+            let copy_regions = [vk::BufferCopy {
+                src_offset: 0,
+                dst_offset: 0,
+                size,
+            }];
+
+            device.cmd_copy_buffer(command_buffer, src_buffer, dst_buffer, &copy_regions);
+
+            device
+                .end_command_buffer(command_buffer)
+                .expect("Failed to end Command Buffer");
+        }
+
+        let submit_info = [vk::SubmitInfo {
+            s_type: vk::StructureType::SUBMIT_INFO,
+            p_next: ptr::null(),
+            wait_semaphore_count: 0,
+            p_wait_semaphores: ptr::null(),
+            p_wait_dst_stage_mask: ptr::null(),
+            command_buffer_count: 1,
+            p_command_buffers: &command_buffer,
+            signal_semaphore_count: 0,
+            p_signal_semaphores: ptr::null(),
+        }];
+
+        unsafe {
+            device
+                .queue_submit(submit_queue, &submit_info, vk::Fence::null())
+                .expect("Failed to Submit Queue.");
+            device
+                .queue_wait_idle(submit_queue)
+                .expect("Failed to wait Queue idle");
+
+            device.free_command_buffers(command_pool, &command_buffers);
+        }
     }
 
     fn find_memory_type(
